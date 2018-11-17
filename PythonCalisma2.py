abadd16adeaf686acc6d8fe29d652c431c006278
Gokberk Hıdıroğlu <gokbey5801@gmail.com>
2018-11-17 23:58:09 +0300

    calisma2

---
+++ PythonCalisma2.py
@@ -0,0 +1,38 @@
+#dizi calismasi
+
+"""dizi= ["Merhaba","Dünya",58,"Sivas"]
+
+for i in dizi:
+    print(i)
+
+kisaltma = input("Kısaltmasını öğrenmek istediğiniz kurum adını girin: ")
+
+for i in kisaltma.split():
+    print(i[0], end="") """
+
+#deneme
+"""
+print("3 kere selam - " * 3)
+print(len("Merhaba dünya"))
+kullaniciAdi="admin"
+parola="asd123"
+t=len(kullaniciAdi)+len(parola)
+print("toplam karakter uzunlugu= ",t) """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
